07b2c5f33745151c7ecfef26f489d6b3db5e4cf2
ercr07001 <ercr07001@E-PW05CGR0>
2024-12-11 12:35:53 +0100

    Add project files.

---
+++ Fraga_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0; 
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Ange heltal nummer {i}:");
+            int number = int.Parse(Console.ReadLine()); 
+            sum += number;
+        }
+
+        Console.WriteLine($"Summan av de inmatade talen är: {sum}");
+
+        Console.ReadKey();
+    }
+}
